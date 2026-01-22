578125f670db30dd83366074fdacafb49fc05335
Bobototo <2275486193@qq.com>
2026-01-22 15:27:37 +0800

    feat:新增员工功能

--- sky-common/src/main/java/com/sky/properties/JwtProperties.java
+++ sky-common/src/main/java/com/sky/properties/JwtProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "sky.jwt")
+@ConfigurationProperties(prefix = "sky.jwt")   //统一前缀，程序自动到
 @Data
 public class JwtProperties {
 
